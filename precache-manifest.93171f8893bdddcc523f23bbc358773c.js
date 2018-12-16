424d5a14615ae4f7efe026cf7b0be71df9c849a2
Yogesh Chaube <ychaube@opentext.com>
2018-12-17 01:01:37 +0530

    Updates

---
+++ precache-manifest.93171f8893bdddcc523f23bbc358773c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/moviedb-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "f81e829081af3af7cf4a",
+    "url": "/moviedb-react/static/css/main.fbaeda31.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/moviedb-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "1bc3faa2bac4f3f42065",
+    "url": "/moviedb-react/static/js/1.1bc3faa2.chunk.js"
+  },
+  {
+    "revision": "c4b3ba5a4819fd84209a",
+    "url": "/moviedb-react/static/js/runtime~main.c4b3ba5a.js"
+  },
+  {
+    "revision": "f81e829081af3af7cf4a",
+    "url": "/moviedb-react/static/js/main.f81e8290.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/moviedb-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/moviedb-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/moviedb-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "1bc3faa2bac4f3f42065",
+    "url": "/moviedb-react/static/css/1.08012e8f.chunk.css"
+  },
+  {
+    "revision": "0b60155890b6839a07e1b5956aad0422",
+    "url": "/moviedb-react/index.html"
+  }
+];
